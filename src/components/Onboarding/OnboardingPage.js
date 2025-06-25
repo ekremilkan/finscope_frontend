cc24c1f9f0efdfe3e3e0331969b239d1e52c27ec
Ahmet Can <can_tanrikulu@icloud.com>
2025-06-26 02:03:44 +0300

    onboard updated

--- src/components/Onboarding/OnboardingPage.js
+++ src/components/Onboarding/OnboardingPage.js
@@ -1,28 +1,17 @@
-import React from 'react';
-import { Image, Text } from 'react-native';
-import { ONBOARDING_CONFIG } from '../../constants/onboardingConstants';
+import { Image, Text, View } from "react-native"
+import { ONBOARDING_CONFIG } from "../../constants/onboardingConstants"
 
 const OnboardingPage = ({ page }) => {
   return {
     backgroundColor: page.backgroundColor,
     image: (
-      <Image
-        source={page.image}
-        style={ONBOARDING_CONFIG.imageSize}
-        resizeMode="contain"
-      />
+      <View style={ONBOARDING_CONFIG.imageContainerStyle}>
+        <Image source={page.image} style={ONBOARDING_CONFIG.imageSize} resizeMode="contain" />
+      </View>
     ),
-    title: (
-      <Text style={ONBOARDING_CONFIG.titleStyle}>
-        {page.title}
-      </Text>
-    ),
-    subtitle: (
-      <Text style={ONBOARDING_CONFIG.subtitleStyle}>
-        {page.subtitle}
-      </Text>
-    ),
-  };
-};
+    title: <Text style={ONBOARDING_CONFIG.titleStyle}>{page.title}</Text>,
+    subtitle: <Text style={ONBOARDING_CONFIG.subtitleStyle}>{page.subtitle}</Text>,
+  }
+}
 
-export default OnboardingPage; 
+export default OnboardingPage
